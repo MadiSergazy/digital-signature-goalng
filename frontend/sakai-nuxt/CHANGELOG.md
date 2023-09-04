88352e887e1dea1e9eaf62ab5697bdab15079d70
Magzhan_Zhumabaev <123943870+MagzhanUnited@users.noreply.github.com>
2023-09-04 18:54:24 +0600

    Remove submodule frontend/sakai-nuxt

--- frontend/sakai-nuxt/CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-# Changelog
-
-## 1.0.0 (2023-05-08)
-
--   The implementation of the Nuxt.js version of the Sakai-Vue project
--- main.go
+++ main.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
 
 	"mado/helpers"
 	"mado/internal"
@@ -14,69 +10,10 @@ import (
 
 const baseURL = "https://sigex.kz"
 
-type ResponseStruct struct {
-	Nonce string `json:"nonce"`
-}
+
 
 // we did't need timeout because when 15min life time of QRCode is expired sigex will message to us
 func main() {
-	nonce, err := MakeRequest()
-	if err != nil {
-		fmt.Println("ERROR OCCURED")
-	}
-	fmt.Println("nonce:", nonce)
-}
-
-func MakeRequest() (string, error) {
-	// Define the URL you want to send the POST request to
-	url := baseURL + "/api/auth"
-
-	// Create a map or struct containing the data you want to send as JSON
-	data := map[string]interface{}{}
-
-	// Convert the data to JSON format
-	jsonData, err := json.Marshal(data)
-	if err != nil {
-		fmt.Println("Error encoding JSON:", err)
-		return "", err
-	}
-
-	// Create a new HTTP request with the POST method, URL, and request body
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
-	if err != nil {
-		fmt.Println("Error creating request:", err)
-		return "", err
-	}
-
-	// Set the request headers if needed (e.g., content-type)
-	req.Header.Set("Content-Type", "application/json")
-
-	// Create an HTTP client and send the request
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println("Error sending request:", err)
-		return "", err
-	}
-	defer resp.Body.Close()
-
-	// Read and process the response
-	responseBody, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Error reading response:", err)
-		return "", err
-	}
-
-	// Print the response status code and body
-	fmt.Println("Status Code:", resp.Status)
-	fmt.Println("Response Body:", string(responseBody))
-	var responseObj ResponseStruct
-	if err := json.Unmarshal(responseBody, &responseObj); err != nil {
-		fmt.Println("Error unmarshaling response:", err)
-		return "", err
-	}
-	nonceValue := responseObj.Nonce
-	return nonceValue, nil
 }
 
 func dinit() {
