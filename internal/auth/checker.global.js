3b57d19d6e0d59c5bc07199405d624d84220f09b
Magzhan_Zhumabaev <123943870+MagzhanUnited@users.noreply.github.com>
2023-09-05 11:49:02 +0600

    lalallal

--- internal/auth/checker.global.js
+++
@@ -1,6 +0,0 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-    if (process.client) return;
-    if (to.path !== '/auth/login' && useCookie('access_token')) {
-        return navigateTo('/auth/login');
-    }
-});
