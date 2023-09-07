0d000a8b5b9098faac9887dc80ad02d7950a2cf7
Madi <saitamenter@gmail.com>
2023-09-07 15:28:52 +0600

    login backend is done

--- backend/internal/auth/auth.go
+++ backend/internal/auth/auth.go
@@ -14,27 +14,27 @@ const (
 	baseUrl = "https://sigex.kz"
 )
 
-func PreparationStep() (*string, *internal.QRSigningClientCMS) {
+func PreparationStep() (egovMobileLink *string, qrSigner *internal.QRSigningClientCMS, nonce *string) {
 
 	body, _ := json.Marshal(map[string]interface{}{})
 
 	response, err := http.Post(baseUrl+"/api/auth", "application/json", bytes.NewBuffer(body))
 	if err != nil {
 		fmt.Println("Error getting аутентификация, подготовительный этап:", err)
-		return nil, nil
+		return nil, nil, nil
 	}
 	defer response.Body.Close()
 
 	if response.StatusCode != http.StatusOK {
 		fmt.Printf("Server returned status '%d: %s\n'", response.StatusCode, response.Status)
-		return nil, nil
+		return nil, nil, nil
 	}
 
 	var responseJSON map[string]interface{}
 	err = json.NewDecoder(response.Body).Decode(&responseJSON)
 	if err != nil {
 		fmt.Println("decode err:", err)
-		return nil, nil
+		return nil, nil, nil
 	}
 	fmt.Println(responseJSON)
 
@@ -43,24 +43,24 @@ func PreparationStep() (*string, *internal.QRSigningClientCMS) {
 		err = qrSigner.AddDataToSign([]string{block, block, block}, nonce, nil, true)
 		if err != nil {
 			fmt.Println("Could not read file: ", err)
-			return nil, nil
+			return nil, nil, nil
 		}
 
 		// Register QR signing
 		dataURL, err := qrSigner.RegisterQRSinging()
 		if err != nil {
 			fmt.Println("RegisterQRSinging Error:", err)
-			return nil, nil
+			return nil, nil, nil
 		}
 		fmt.Println("First man RegisterQRSinging dataURL: ", dataURL)
 
 		eGovMobileLaunchLink := qrSigner.GetEGovMobileLaunchLink()
 		fmt.Println("For log-in eGov Mobile Launch Link:", eGovMobileLaunchLink) //todo pull to front
 		//todo response front
-		return &eGovMobileLaunchLink, qrSigner
+		return &eGovMobileLaunchLink, qrSigner, &nonce
 	} else {
 		fmt.Println("err did't have nonce in resp:", err)
-		return nil, nil
+		return nil, nil, nil
 	}
 
 }
